b267d9d0eadcad382522850915bb58d2dd65f508
Lezou Dali <lezou.dali@gmail.com>
2017-01-21 16:28:37 -0500

    python -m pytest is a-ok

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,35 @@
+import os
+
 import pytest
 
+from db.core.utils import create_engine, get_scoped_session
+
 pytest_plugins = []
+
+
+@pytest.fixture(scope='session')
+def connection_params():
+    return {
+        'host': 'postgres',
+        'port': 5432,
+        'database': 'stylishly-test',
+        'username': os.getenv('TEST_DB_USERNAME', 'stylishly'),
+        'password': os.getenv('TEST_DB_PASSWORD', 'stylishly')
+    }
+
+
+@pytest.fixture(scope='function')
+def session(connection_params, request):
+    """Returns a SQLA database session"""
+    engine = create_engine(**connection_params)
+    connection = engine.connect()
+
+    Session = get_scoped_session(connection)
+    sess = Session()
+
+    transaction = connection.begin()
+
+    yield sess
+
+    transaction.rollback()
+    connection.close()
